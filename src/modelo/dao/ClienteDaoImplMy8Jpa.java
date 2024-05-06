5908a24c5e2b76caefa98b67772fbe018edd03f7
rubenperaita <ruben.peraita.moreno@gmail.com>
2024-05-06 12:20:52 +0200

    eliminar

---
+++ src/modelo/dao/ClienteDaoImplMy8Jpa.java
@@ -0,0 +1,41 @@
+package modelo.dao;
+
+import java.util.List;
+
+import modelo.entidades.Cliente;
+
+public class ClienteDaoImplMy8Jpa extends AbstractDaoImplMy8Jpa implements ClienteDao{
+
+	@Override
+	public boolean alta(Cliente obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean eliminar(String clave) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean modificar(Cliente obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Cliente buscarUno(String clave) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Cliente> buscarTodos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+	
+}
---
+++ src/modelo/dao/GestionClientes.java
@@ -0,0 +1,9 @@
+package modelo.dao;
+
+public class GestionClientes {
+	
+	public static void main(String[] args) {
+		
+	}
+	
+}
