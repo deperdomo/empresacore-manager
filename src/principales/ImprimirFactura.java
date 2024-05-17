8b45bc79b1ecbedb307c6f917eb49ced92b223fe
Deivi Perdomo <deivi54357326@gmail.com>
2024-05-17 18:40:33 +0200

    Factura Terminada

--- src/principales/ImprimirFactura.java
+++ src/principales/ImprimirFactura.java
@@ -1,10 +1,6 @@
 package principales;
 
 
-import java.util.ArrayList;
-
-import org.hibernate.mapping.List;
-
 import modelo.dao.ClienteDao;
 import modelo.dao.ClienteDaoImplMy8Jpa;
 import modelo.dao.FacturaDao;
@@ -16,10 +12,10 @@ import modelo.dao.ProyectoConProductoDaoImplMy8Jpa;
 import modelo.dao.ProyectoDao;
 import modelo.dao.ProyectoDaoImplMy8Jpa;
 import modelo.entidades.Cliente;
-import modelo.entidades.Empleado;
 import modelo.entidades.Factura;
 import modelo.entidades.Proyecto;
 import modelo.entidades.ProyectoConEmpleado;
+import modelo.entidades.ProyectoConProducto;
 
 public class ImprimirFactura {
 	
@@ -68,14 +64,13 @@ public class ImprimirFactura {
 		System.out.println("Datos del CLIENTE: ");
 		System.out.print("nombre: " + cliente.getNombre());
 		System.out.println("    Dirección: "+ cliente.getDomicilio());
-		//System.out.print("");
 		
 		System.out.println("");
 		
 		System.out.println("Datos del PROYECTO: ");
 		System.out.println("Código del Proyecto: " + proyecto.getIdProyecto());
 		System.out.println("Descripción Proyecto: "+ proyecto.getDescripcion());
-		System.out.println("Fecha Inisio: "+ proyecto.getFechaInicio() + "    " + proyecto.getFechaFinReal());
+		System.out.println("Fecha Inicio: "+ proyecto.getFechaInicio() + "    Fecha Fin real: " + proyecto.getFechaFinReal());
 		
 		System.out.println("");
 		
@@ -85,20 +80,32 @@ public class ImprimirFactura {
 		
 		System.out.println("LISTA EMPLEADOS");
 		
+		double totalPrecio = 0;
 		for (ProyectoConEmpleado l : pcedao.empleadosByProyecto(id_proyecto)) {
-			System.out.print("Apellido, nombre"+l.getEmpleado().getApellidos()+", "+l.getEmpleado().getNombre());
+			totalPrecio += l.getHorasAsignadas() * l.getEmpleado().getPerfil().getTasaStandard().doubleValue();
+			System.out.print("Apellido, nombre "+l.getEmpleado().getApellidos()+", "+l.getEmpleado().getNombre());
 			System.out.print("    Horas: "+l.getHorasAsignadas());
-			System.out.print("    Importe repercutido:  "+(l.getHorasAsignadas() * (int)l.getEmpleado().getPerfil().getTasaStandard()));
+			System.out.println("    Importe repercutido:  "+(l.getHorasAsignadas() * l.getEmpleado().getPerfil().getTasaStandard().doubleValue()));
 		}
-		
-		System.out.println("");
-		System.out.println("Total Horas: "+pcedao.horasAsignadasAProyecto(id_proyecto)+ "    Toal Precio: " + pcedao.costeActualDeProyecto(id_proyecto));
+		System.out.print("Total Horas: " + pcedao.horasAsignadasAProyecto(id_proyecto));
+		System.out.println("    Total Precio: "+ totalPrecio);
 		
 		System.out.println("");
 		
 		System.out.println("LISTA PRODUCTOS");
-		// hacer un metdo en proyectoConProductoDao que me traiga todos los productos asignados a un proyecto
-		System.out.println(pcpdao.productosByProyecto(id_proyecto));
+		
+		double totalPrecioProductos = 0;
+		int totalProductos = 0;
+		for (ProyectoConProducto l : pcpdao.productosByProyecto(id_proyecto)) {  
+		        totalProductos += l.getCantidad();
+		        totalPrecioProductos += l.getPrecioAsignado() * l.getCantidad();
+		        System.out.print("Descripcion: " + l.getProducto().getDescripcion());
+		        System.out.print("    Cantidad: " + l.getCantidad());
+		        System.out.print("    Precio por uno: " + l.getPrecioAsignado());
+		        System.out.println("    Total: " + l.getPrecioAsignado() * l.getCantidad());
+		        System.out.print("Total productos: " + totalProductos);
+		        System.out.println("    Total pagado por productos: " + totalPrecioProductos);
+		}
 		
 		
 		System.out.println("");
